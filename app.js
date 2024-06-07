2b42ad8e34d84097434f1339c8f113d9ed0dfe6d
Muskanjadhav <muskanjadhav109@gmail.com>
2024-06-08 00:26:56 +0530

    add files

---
+++ app.js
@@ -0,0 +1,71 @@
+let key= "ba32026504eb471292eb243aa0c978b0";
+let cardData = document.querySelector(".cardData");
+let searchBtn = document.getElementById("searchBtn");
+let inputData = document.getElementById("inputData");
+let searchType = document.getElementById("type");
+
+const getData = async(input) => {
+    let res = await fetch(`https://newsapi.org/v2/everything?q=${input}&apiKey=${key}`);
+    let jsonData = await res.json();
+    console.log(jsonData.articles);
+
+    searchType.innerHTML = "Search : "+ input;
+    inputData.value = "";
+    cardData.innerHTML = "";
+    jsonData.articles.forEach(function(article){
+        console.log(article);
+
+    let divs = document.createElement("div");
+    divs.classList.add("card");
+    cardData.appendChild(divs);
+
+    divs.innerHTML = `
+    <img src="${article.urlToImage}" alt="">
+    <h3>${article.title}</h3>
+    <p>${article.description}</p>    
+    `
+    divs.addEventListener("click", function(){
+        window.open(article.url);
+    });
+    })
+}
+
+window.addEventListener("load", function(){
+    getData("India");
+});
+
+searchBtn.addEventListener("click", function(){
+    let inputText = inputData.value;
+    getData(inputText);
+})
+
+function navClick(navName){
+    if(navName == "politics"){
+        document.getElementById("politics").style.color = "rgb(0,140,155)";
+        document.getElementById("sports").style.color = "white";
+        document.getElementById("technology").style.color = "white";
+        document.getElementById("ipl").style.color = "white";
+
+    }
+
+    if(navName == "sports"){
+        document.getElementById("politics").style.color = "white";
+        document.getElementById("sports").style.color = "rgb(0,140,155)";
+        document.getElementById("technology").style.color = "white";
+        document.getElementById("ipl").style.color = "white";
+    }
+
+    if(navName == "technology"){
+        document.getElementById("politics").style.color = "white";
+        document.getElementById("sports").style.color = "white";
+        document.getElementById("technology").style.color = "rgb(0,140,155)";
+        document.getElementById("ipl").style.color = "white";
+    }
+    if(navName == "ipl"){
+        document.getElementById("politics").style.color = "white";
+        document.getElementById("sports").style.color = "white";
+        document.getElementById("technology").style.color = "white";
+        document.getElementById("ipl").style.color = "rgb(0,140,155)";
+    }
+    getData(navName);
+}
